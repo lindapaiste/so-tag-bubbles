226274fbb6779a214501defb172c4dc1af37a7e4
Linda Paiste <lindapaiste@gmail.com>
2021-06-02 19:22:48 -0500

    Attempting to fix build errors

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module "*.module.css" {
-  const classes: { [key: string]: string; };
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+declare module '*.css' {
+  const classes: { [key: string]: string };
   export default classes;
 }
